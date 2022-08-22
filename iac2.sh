0fa895ac4c7235a16cff6645137c245b99155bbb
Weverson <weversondf@gmail.com>
2022-08-22 15:54:00 -0300

    Add files via upload
    Infraestrutura como Código - Script de Provisionamento de um Servidor Web (Apache)

---
+++ iac2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Atualizando o servidor..."
+apt update
+apt upgrade -y
+apt install apache2 -y
+apt install unzip -y
+
+
+echo "Baixando e copiando os arquivos da aplicação..."
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
+
+
